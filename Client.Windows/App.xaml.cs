af2a0d1026d320af2ba7fb8d050aee1204e3b40d
Mathieu MACK <mathieu.mack@live.fr>
2017-01-16 09:32:39 +0100

    Fix nuspec file and CI builds

--- Client.Windows/App.xaml.cs
+++ Client.Windows/App.xaml.cs
@@ -20,7 +20,7 @@ namespace Client.Windows
         {
             base.OnStartup(e);
             App.hockeyClient = new HockeyClientWpf();
-            App.hockeyClient.Configure("$APIKey here", "1.0.0.0", true, true, true);
+            App.hockeyClient.Configure("SetApiKeyHere", "1.0.0.0", true, true, true);
             
             App.hockeyClient.TrackEvent("Start application from HockeyApp");
 
