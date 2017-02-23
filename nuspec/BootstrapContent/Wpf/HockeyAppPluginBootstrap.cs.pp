e82344e4ea57c10926be7be05bb314b4fa9416e0
MACK Mathieu <mathieu.mack@LIVE.FR>
2017-02-23 21:49:16 +0100

    Fix UWP nuget configuration

---
+++ nuspec/BootstrapContent/Wpf/HockeyAppPluginBootstrap.cs.pp
@@ -0,0 +1,9 @@
+using MvvmCross.Platform.Plugins;
+
+ namespace $rootnamespace$.Bootstrap
+ {
+    public class HockeyAppPluginBootstrap
+        : MvxPluginBootstrapAction<MvvX.Plugins.HockeyApp.PluginLoader>
+    {
+    }
+ }
