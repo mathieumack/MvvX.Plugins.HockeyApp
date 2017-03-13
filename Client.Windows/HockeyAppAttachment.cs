0760d8155dbce7048b52b9cfdf546f14471bb245
FredericGinoux <frederic.ginoux@gmail.com>
2017-03-13 15:21:04 +0100

    Added SendFeedbackAsync method for wpf interface

---
+++ Client.Windows/HockeyAppAttachment.cs
@@ -0,0 +1,20 @@
+﻿using MvvX.Plugins.HockeyApp;
+
+namespace Client.Windows
+{
+    public class HockeyAppAttachment : IHockeyAppAttachment
+    {
+        //
+        // Summary:
+        //     Mime content type
+        public string ContentType { get; set; }
+        //
+        // Summary:
+        //     Bytes (usually only used when uploading attachments)
+        public byte[] DataBytes { get; set; }
+        //
+        // Summary:
+        //     Name of the file when it was uploaded
+        public string FileName { get; set; }
+    }
+}
