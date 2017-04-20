f4896e60ce6792ee485ef245246c42c3887d5e4c
FredericGinoux <frederic.ginoux@gmail.com>
2017-04-20 15:01:31 +0200

    Update of the SendFeedbackAsync method to return the thread informations (#5)
    * Added SendFeedbackAsync method for wpf interface
    
    * Updated SendFeedbackAsync method to return the thread creation date, id, status and token.

---
+++ MvvX.Plugins.HockeyApp.Wpf/HockeyAppThread.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.HockeyApp;
+
+namespace MvvX.Plugins.HockeyApp.Wpf
+{
+    public class HockeyAppThread : IHockeyAppThread
+    {
+        /// <summary>
+        /// time of creation as string
+        /// </summary>
+        public string CreatedAt { get; set; }
+
+        /// <summary>
+        /// unique id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// status
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// unique token for this thread
+        /// </summary>
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="thread"></param>
+        public HockeyAppThread(IFeedbackThread thread)
+        {
+            CreatedAt = thread.CreatedAt;
+            Id = thread.Id;
+            Status = thread.Status;
+            Token = thread.Token;
+        }
+    }
+}
